28598bbb724bdd7a570bb8a71de9f477cb0fd3a5
ErickDKnight <erickmontes_76@hotmail.com>
2022-12-11 12:44:45 -0600

    fixed frecuence

--- JS-Challenge-2/frecuent.js
+++ JS-Challenge-2/frecuent.js
@@ -1,22 +1,26 @@
 let arr1= [3, 'c', 'c', 'a', 2, 3, 'c', 2, 4, 9, 9]
 
 function frecuence (arr){
-    let biggestFr=0
-    let element=0
-    for(let i=0; i<arr.length; i){
-        let frecuencia = 0
-        for(let j=i; j<arr1.length; j++){
-            if(arr1[i]===arr1[j]){
-                frecuencia++
-            }
+    let leastFr=0
+    let element
+
+    for(let i=0; i<arr.length; i++){
+        let sameElements = arr.filter((element)=> element === arr[i])
+        let frecuence = sameElements.length
+
+        if(i === 0){
+            leastFr=frecuence
+            element = arr[i]
         }
 
-        if(frecuencia>biggestFr){
-            biggestFr = frecuencia
-            element = i
+        if(frecuence < leastFr){
+            leastFr = frecuence
+            element = arr[i]
+        }else if(frecuence === leastFr){
+            element = element + ', ' + arr[i]
         }
+
     }
-    return arr1[i]
+    return element
 }
-
 console.log(frecuence(arr1))
