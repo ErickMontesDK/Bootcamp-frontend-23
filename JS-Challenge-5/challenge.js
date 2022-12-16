fe1ca517a567e9d832e42032fc1413bad393ad39
ErickDKnight <erickmontes_76@hotmail.com>
2022-12-16 09:20:00 -0600

    challenge 5

---
+++ JS-Challenge-5/challenge.js
@@ -0,0 +1,34 @@
+class HttpError extends Error {
+    constructor(response){
+        super(`${response.status} for ${response.url}`);
+        this.name = 'HttpError'
+        this.response = response;
+    }
+}
+
+
+async function loadJson(url){
+    const response = await fetch(url)
+    if(response.status === 200) {
+        return response.json();
+    } else{
+        throw new HttpError(response)
+    }
+}
+
+async function demoGithubUser(){
+    let name = "ErickMontesDK"
+    
+    try{
+        const user = await loadJson(`https://api.github.com/users/${name}`)
+        console.log(`Full name: ${user.name}`)
+        return user
+    }catch(err){
+        if(err instanceof HttpError && err.status == 404){
+            console.log(`No such user, please reenter`)
+        }else {
+            throw err
+        }
+    }
+}
+demoGithubUser()
