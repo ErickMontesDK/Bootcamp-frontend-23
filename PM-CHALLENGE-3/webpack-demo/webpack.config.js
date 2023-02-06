5c8c936bb5b3d5763b5b38816bc16c9889874113
ErickDKnight <erickmontes_76@hotmail.com>
2023-02-06 15:50:37 -0600

    added webpack-dev-server

--- PM-CHALLENGE-3/webpack-demo/webpack.config.js
+++ PM-CHALLENGE-3/webpack-demo/webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+ 
 module.exports = {
-    mode: "none",
-    entry: './src/index.js',
+    mode: 'development',
+    entry: {
+        app: './src/index.js',
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
-    }
+        clean: false,
+    },
+    module: {
+    rules: [
+        {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader' ],
+        },
+    ],
+    },
+    plugins: [
+    new HtmlWebpackPlugin({
+        template: './index.html'
+    })
+    ]
 };
