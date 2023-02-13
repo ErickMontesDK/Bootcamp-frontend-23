178609eb4a6e6b3f4b411ebe07b1f681ad6178c6
ErickDKnight <erickmontes_76@hotmail.com>
2023-02-12 18:58:34 -0600

    added pokedex

--- PM-CHALLENGE-3/webpack-demo/webpack.dev.js
+++ PM-CHALLENGE-3/webpack-demo/webpack.dev.js
@@ -3,8 +3,11 @@ const path = require('path');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-mode: 'development',
-output: {
-    path: path.resolve(__dirname, 'dist'),
-    }
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        },
+    devServer: {
+        port: 5000
+      }
 });
